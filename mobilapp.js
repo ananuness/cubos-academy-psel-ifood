bf8d02ef1b951e92423b31586f5d9dd60eeb1812
Ana Nunes <beatriznunez601@gmail.com>
2021-06-08 21:50:46 -0300

    Mobilapp

---
+++ mobilapp.js
@@ -0,0 +1,16 @@
+function solucao(tempo, distancia) {
+    if(tempo < 5){
+        console.log(600);
+    }
+    else if(tempo >= 5 && tempo <= 60){
+        console.log(tempo*100 + distancia*50);
+    }
+    else{
+        if(distancia < 100){
+            console.log(distancia*200);
+        }
+        else{
+            console.log(distancia*150);
+        }
+    }
+}
